dbb4ebc1512beee75e45407073174f8b9f100795
vladimir.z <vladimir.z@indriver.com>
2021-08-09 16:56:11 +0900

    counter in fragment 1.2

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -31,7 +31,6 @@ class MainActivity : AppCompatActivity() {
 
     fun counterMeFragment(view: View) {
         val fragment: ExFragment = supportFragmentManager.findFragmentById(R.id.exampleFragment) as ExFragment
-
     }
 
 
