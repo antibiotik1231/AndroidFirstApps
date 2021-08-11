8f1260eb519656c5406a8c0a9146e20b2c838c29
vladimir.z <vladimir.z@indriver.com>
2021-08-11 15:54:54 +0900

    Dagger+Ciceron+View binding

--- app/src/main/java/com/example/myfirstapp/ExFragment.kt
+++ app/src/main/java/com/example/myfirstapp/ExFragment.kt
@@ -11,12 +11,13 @@ import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import com.example.myfirstapp.databinding.ExampleFragmentBinding
 import com.example.myfirstapp.di.getAppComponent
 import com.github.terrakok.cicerone.Router
 import java.util.*
 import javax.inject.Inject
 
-class ExFragment : Fragment() {
+class ExFragment : Fragment(R.layout.example_fragment) {
 
     companion object {
         const val ARG_NUMBER = "number"
@@ -32,43 +33,30 @@ class ExFragment : Fragment() {
     @Inject
     lateinit var router: Router
 
+    private lateinit var binding: ExampleFragmentBinding
+
     override fun onAttach(context: Context) {
         context.getAppComponent().inject(this)
         super.onAttach(context)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view: View? = inflater.inflate(R.layout.example_fragment, container, false)
-        return view
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(view.findViewById<TextView>(R.id.textView3)) {
-            text = (activity as MainActivity).getChain()
-        }
-        with(view.findViewById<Button>(R.id.button2)) {
-            setOnClickListener {
+        val chainText = (activity as MainActivity).getChain()
+        binding = ExampleFragmentBinding.bind(view)
+        with(binding) {
+            textView3.text = chainText
+            button2.setOnClickListener {
                 router.navigateTo(Screens.ExScreen(number + 1))
             }
-        }
-        with(view.findViewById<Button>(R.id.button3)) {
-            setOnClickListener {
+            button3.setOnClickListener {
                 router.exit()
             }
-        }
-        with(view.findViewById<Button>(R.id.button4)) {
-            setOnClickListener {
-                var message: String = view.findViewById<TextView>(R.id.textView3).text.toString()
-                router.navigateTo(Screens.StartScreen(message))
+            button4.setOnClickListener {
+                router.navigateTo(Screens.StartScreen(chainText))
             }
-        }
-        with(view.findViewById<Button>(R.id.button5)) {
-            setOnClickListener {
-                val num = activity?.supportFragmentManager?.backStackEntryCount!!.toString()
+            button5.setOnClickListener {
+                val num = (number + 1).toString()
                 val randomNumber = let { Random().nextInt(num.toInt()) }.toString()
                 router.navigateTo(Screens.RandomScreen(randomNumber, num))
             }
